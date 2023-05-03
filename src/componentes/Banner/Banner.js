d66498f1803bc78b29710c0f1e9187f455cdca91
Kaio Felipe <110945125+oi-Kaiofelipe07@users.noreply.github.com>
2023-05-03 10:43:01 -0300

    Update Banner.js

--- src/componentes/Banner/Banner.js
+++ src/componentes/Banner/Banner.js
@@ -4,7 +4,7 @@ export const Banner = () => {
     // JSX
     return (
         <header className="banner">
-            <img src="/imagens/Banner.png" alt="O banner principal da página de gamer"/>
+            <img src="/Imagens/Banner.png" alt="O banner principal da página de gamer"/>
         </header>
     )
 }
